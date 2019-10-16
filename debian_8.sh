fff7884715f8ca2db9d18f92e699fe4447371417
Demi <kizura.vian@gmail.com>
2019-10-16 10:53:25 +0800

    Update debian_8.sh

--- debian_8.sh
+++ debian_8.sh
@@ -34,9 +34,9 @@ echo " "
 
 sudo apt-get install expect -y && apt-get -y update && sudo apt-get -y upgrade
 sudo apt-get install checkinstall build-essential -y
-wget http://www.softether-download.com/files/softether/v4.27-9668-beta-2018.05.29-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-x64-64bit.tar.gz
-tar -zxf softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-x64-64bit.tar.gz
-rm -rf softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-x64-64bit.tar.gz
+wget http://www.softether-download.com/files/softether/v4.30-9696-beta-2019.07.08-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.30-9696-beta-2019.07.08-linux-x64-64bit.tar.gz
+tar -zxf softether-vpnserver-v4.30-9696-beta-2019.07.08-linux-x64-64bit.tar.gz
+rm -rf softether-vpnserver-v4.30-9696-beta-2019.07.08-linux-x64-64bit.tar.gz
 cd /root/vpnserver && expect -c 'spawn make; expect number:; send 1\r; expect number:; send 1\r; expect number:; send 1\r; interact'
 cd && mv vpnserver/ /usr/local && chmod 600 * /usr/local/vpnserver/ && chmod 700 /usr/local/vpnserver/vpncmd && chmod 700 /usr/local/vpnserver/vpnserver
 echo '#!/bin/sh
