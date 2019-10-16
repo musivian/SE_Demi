9da61df475c40fbd094395cbf645da00a7f4712d
Demi <kizura.vian@gmail.com>
2019-10-16 11:03:19 +0800

    Update debian_8.sh

--- debian_8.sh
+++ debian_8.sh
@@ -32,8 +32,8 @@ echo " "
 echo "Now sit back and wait until the installation finished."
 echo " "
 
-sudo apt-get install expect -y && apt-get -y update && sudo apt-get -y upgrade
-sudo apt-get install checkinstall build-essential -y
+apt-get install expect -y && apt-get -y update && sudo apt-get -y upgrade
+apt-get install checkinstall build-essential -y
 wget http://www.softether-download.com/files/softether/v4.30-9696-beta-2019.07.08-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.30-9696-beta-2019.07.08-linux-x64-64bit.tar.gz
 tar -zxf softether-vpnserver-v4.30-9696-beta-2019.07.08-linux-x64-64bit.tar.gz
 rm -rf softether-vpnserver-v4.30-9696-beta-2019.07.08-linux-x64-64bit.tar.gz
